e49b77a6cfdf19f4ed93ce4ab2c1244529bc93d1
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2022-05-31 22:25:26 -0300

    feat: add tweet component

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,41 @@
+import { HeartIcon } from '@heroicons/react/outline'
+
+export const Tweet = ({ name, username, avatar, likes, children }) => {
+  return (
+    <div className="flex space-x-3 p-4 border-b border-silver">
+      <div>
+        <img src={avatar}/>
+      </div>
+      <div className='space-y-1'>
+        <span className="font-bold text-sm">{name}</span>{' '}
+        <span className="text-sm text-silver">@{username}</span>
+
+        <p>{children}</p>
+
+        <div className='flex space-x-1 text-silver items-center'>
+          <HeartIcon className='w-6 stroke-1'/>
+          <span>{likes}</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export const App = () => {
   return (
-    <div>Olá mundo!</div>
+    <>
+      <Tweet
+        name="Elon Musk" username="elonmusk"
+        avatar="/src/avatar.png" likes="1.2M"
+      >
+        Let's manipulate crypto!
+      </Tweet>
+      <Tweet
+        name="Vinícius Godoy" username="vinicius-godoy"
+        avatar="/src/avatar.png" likes="6"
+      >
+        Let's study ReactJS!
+      </Tweet>
+    </>
   )
 }
