3a431d26a392cfb0ad3b32bc08380e4324f7fcc0
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2022-05-31 22:22:57 -0300

    chore: add brand colors to tailwind config file

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,21 @@
+const brandColors = {
+  birdBlue: '#1D9BF0',
+  platinum: '#E7E9EA',
+  silver: '#71767B',
+  onix: '#333639',
+  richBlack: '#15202B',
+}
+
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...brandColors,
+        backgroundColor: brandColors.richBlack,
+        textColor: brandColors.platinum,
+      }
+    },
   },
   plugins: [],
 }
