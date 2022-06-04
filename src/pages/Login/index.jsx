b9a8e1e93e142a75f22f60cfc0b63d58a45a1832
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2022-06-04 14:34:17 -0300

    style: add brand section and resposive design to login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -33,57 +33,63 @@ export const Login = ({ signInUser }) => {
   })
 
   return (
-    <div className="flex flex-col h-full justify-center p-12 space-y-6">
-      <h1 className="text-3xl">Acesse sua conta</h1>
+    <div className="flex h-full justify-center ">
+      <div className="bg-birdBlue lg:flex-1"></div>
 
-      <form className="space-y-6" onSubmit={formik.handleSubmit}>
+      <div className="flex-1 flex justify-center items-center p-12 space-y-6">
+        <div className="max-w-md flex-1">
+          <h1 className="text-3xl">Acesse sua conta</h1>
 
-        <div className="space-y-2">
-          <Input
-            name="email"
-            type="text"
-            placeholder="E-mail"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            disable={formik.isSubmitting}
-          />
-          {(formik.touched.email && formik.errors.email) && (
-            <span className="text-red-500 text-sm">
-              {formik.errors.email}
-            </span>
-          )}
+          <form className="space-y-6" onSubmit={formik.handleSubmit}>
+
+            <div className="space-y-2">
+              <Input
+                name="email"
+                type="text"
+                placeholder="E-mail"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                disable={formik.isSubmitting}
+              />
+              {(formik.touched.email && formik.errors.email) && (
+                <span className="text-red-500 text-sm">
+                  {formik.errors.email}
+                </span>
+              )}
+            </div>
+
+            <div className="space-y-2">
+              <Input
+                name="password"
+                type="password"
+                placeholder="Senha"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                disable={formik.isSubmitting}
+              />
+              {(formik.touched.password && formik.errors.password) && (
+                <span className="text-red-500 text-sm">
+                  {formik.errors.password}
+                </span>
+              )}
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-birdBlue py-4 rounded-full disabled:opacity-50 text-lg"
+              disabled={!formik.isValid || formik.isSubmitting}
+            >
+              {formik.isSubmitting ? 'Enviando...' : 'Entrar'}
+            </button>
+          </form>
+
+          <span className="text-sm text-silver text-center">
+            Não tem conta? <a href="/signup" className="text-birdBlue">Inscreva-se</a>
+          </span>
         </div>
-
-        <div className="space-y-2">
-          <Input
-            name="password"
-            type="password"
-            placeholder="Senha"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            disable={formik.isSubmitting}
-          />
-          {(formik.touched.password && formik.errors.password) && (
-            <span className="text-red-500 text-sm">
-              {formik.errors.password}
-            </span>
-          )}
-        </div>
-
-        <button
-          type="submit"
-          className="w-full bg-birdBlue py-4 rounded-full disabled:opacity-50 text-lg"
-          disabled={!formik.isValid || formik.isSubmitting}
-        >
-          {formik.isSubmitting ? 'Enviando...' : 'Entrar'}
-        </button>
-      </form>
-
-      <span className="text-sm text-silver text-center">
-        Não tem conta? <a href="/signup" className="text-birdBlue">Inscreva-se</a>
-      </span>
+      </div>
     </div>
   )
 }
