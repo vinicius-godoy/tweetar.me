bed2325b421582c45f1a9447db24c9581efbe5e2
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2022-06-05 23:43:44 -0300

    style: improve responsive desktop design on sign up page

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -35,91 +35,93 @@ export const SignUp = ({ signInUser }) => {
   })
 
   return (
-    <div className="flex flex-col h-full justify-center p-12 space-y-6">
-      <h1 className="text-3xl">Crie sua conta</h1>
+    <div className="flex h-full justify-center items-center p-12">
+      <div className="flex flex-col space-y-6 w-full lg:w-1/2">
+        <h1 className="text-3xl">Crie sua conta</h1>
 
-      <form className="space-y-6" onSubmit={formik.handleSubmit}>
+        <form className="space-y-6" onSubmit={formik.handleSubmit}>
 
-        <div className="space-y-2">
-          <Input
-            name="name"
-            type="text"
-            placeholder="Nome"
-            value={formik.values.name}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            disable={formik.isSubmitting}
-          />
-          {(formik.touched.name && formik.errors.name) && (
-            <span className="text-red-500 text-sm">
-              {formik.errors.name}
-            </span>
-          )}
-        </div>
+          <div className="space-y-2">
+            <Input
+              name="name"
+              type="text"
+              placeholder="Nome"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              disable={formik.isSubmitting}
+            />
+            {(formik.touched.name && formik.errors.name) && (
+              <span className="text-red-500 text-sm">
+                {formik.errors.name}
+              </span>
+            )}
+          </div>
 
-        <div className="space-y-2">
-          <Input
-            name="username"
-            type="text"
-            placeholder="Nome de Usuário"
-            value={formik.values.username}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            disable={formik.isSubmitting}
-          />
-          {(formik.touched.username && formik.errors.username) && (
-            <span className="text-red-500 text-sm">
-              {formik.errors.username}
-            </span>
-          )}
-        </div>
+          <div className="space-y-2">
+            <Input
+              name="username"
+              type="text"
+              placeholder="Nome de Usuário"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              disable={formik.isSubmitting}
+            />
+            {(formik.touched.username && formik.errors.username) && (
+              <span className="text-red-500 text-sm">
+                {formik.errors.username}
+              </span>
+            )}
+          </div>
 
-        <div className="space-y-2">
-          <Input
-            name="email"
-            type="text"
-            placeholder="E-mail"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            disable={formik.isSubmitting}
-          />
-          {(formik.touched.email && formik.errors.email) && (
-            <span className="text-red-500 text-sm">
-              {formik.errors.email}
-            </span>
-          )}
-        </div>
+          <div className="space-y-2">
+            <Input
+              name="email"
+              type="text"
+              placeholder="E-mail"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              disable={formik.isSubmitting}
+            />
+            {(formik.touched.email && formik.errors.email) && (
+              <span className="text-red-500 text-sm">
+                {formik.errors.email}
+              </span>
+            )}
+          </div>
 
-        <div className="space-y-2">
-          <Input
-            name="password"
-            type="password"
-            placeholder="Senha"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            disable={formik.isSubmitting}
-          />
-          {(formik.touched.password && formik.errors.password) && (
-            <span className="text-red-500 text-sm">
-              {formik.errors.password}
-            </span>
-          )}
-        </div>
+          <div className="space-y-2">
+            <Input
+              name="password"
+              type="password"
+              placeholder="Senha"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              disable={formik.isSubmitting}
+            />
+            {(formik.touched.password && formik.errors.password) && (
+              <span className="text-red-500 text-sm">
+                {formik.errors.password}
+              </span>
+            )}
+          </div>
 
-        <button
-          type="submit"
-          className="w-full bg-birdBlue py-4 rounded-full disabled:opacity-50 text-lg"
-          disabled={!formik.isValid || formik.isSubmitting}
-        >
-          {formik.isSubmitting ? 'Enviando...' : 'Cadastrar'}
-        </button>
-      </form>
+          <button
+            type="submit"
+            className="w-full bg-birdBlue py-4 rounded-full disabled:opacity-50 text-lg"
+            disabled={!formik.isValid || formik.isSubmitting}
+          >
+            {formik.isSubmitting ? 'Enviando...' : 'Cadastrar'}
+          </button>
+        </form>
 
-      <span className="text-sm text-silver text-center">
-        Já tem uma conta? <a href="/login" className="text-birdBlue">Acesse</a>
-      </span>
+        <span className="text-sm text-silver text-center">
+          Já tem uma conta? <a href="/login" className="text-birdBlue">Acesse</a>
+        </span>
+      </div>
     </div>
   )
 }
